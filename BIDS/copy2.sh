69e0a09ec9d46731d5f63b28bb3ea2b2deff32a4
munoztd0 <david.munoz@etu.unige.ch>
2020-04-07 22:47:00 +0200

    second

--- BIDS/copy2.sh
+++ BIDS/copy2.sh
@@ -3,25 +3,34 @@ home=$(eval echo ~$user)
 
 #small function to move and copy files
 subjID=$1
+group=$2
+
+source ~/anaconda3/etc/profile.d/conda.sh 
+conda activate NEW
+
 
 #for subjID in 01 02 03 04 05 06 07 09 10 11 12 13 14 15 16 17 18 20 21 22 23 24 25 26
   #do
-  for taskID in hedonic PIT
-    do
-  #mkdir ${home}/REWOD/SOURCEDATA/behav/${subj}
-  #mv ${home}/REWOD/SOURCEDATA/physio/s${subj}* ${home}/REWOD/SOURCEDATA/physio/${subj}/
-  
-  #gzip ${home}/REWOD/DERIVATIVES/PREPROC/sub-${subjID}/ses-second/func/sub-${subjID}_task* 
-  #rm ${home}/REWOD/sub-${subjID}/ses-second/func/*.nii.gz
-    cp ${home}/REWOD/DERIVATIVES/PREPROC/sub-${subjID}/ses-second/func/sub-${subjID}_task-${taskID}_run-01_UnsmoothedBold.nii.gz ${home}/REWOD/sub-${subjID}/ses-second/func/sub-${subjID}_ses-second_task-${taskID}_run-01_bold.nii.gz
-  #cp ${home}/REWOD/DERIVATIVES/PREPROC/sub-${subj}/ses-second/func/sub-${subj}_task-taskID_run-01_smoothBold.nii /home/cisa/REWOD/DERIVATIVES/ANALYSIS/CONN/func_PIT/
-  #mkdir /home/cisa/REWOD/DERIVATIVES/ANALYSIS/CONN/1stLEVEL/sub-${subj}
-  #mv ${home}/REWOD/DERIVATIVES/PREPROC/sub-${subj}/ses-first/behav ${home}/REWOD/DERIVATIVES/PREPROC/sub-${subj}/ses-first/beh
+  #for taskID in hedonic PIT
+    #do
+#1mkdir -p ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat
+#1cp ${home}/OBIWAN/DATA/STUDY/SOURCEDATA/sub-${group}${subjID}/ses-first/anat/*.json ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/
+#mkdir -p ${home}/OBIWAN/sub-${group}${subjID}/ses-second/func
+#cp ${home}/OBIWAN/DATA/STUDY/SOURCEDATA/sub-${group}${subjID}/ses-second/func/*.json ${home}/OBIWAN/sub-${group}${subjID}/ses-second/func/
+#cp ${home}/OBIWAN/DATA/STUDY/SOURCEDATA/sub-${group}${subjID}/ses-second/func/*.tsv ${home}/OBIWAN/sub-${group}${subjID}/ses-second/func/
+#sub-${group}${subjID}_ses-second_acq-task-hedonicreactivity_magnitude1.nii.gz
+#sub-${group}${subjID}_ses-second_run-01_magnitude.nii.gz
+#cp ${home}/OBIWAN/DATA/STUDY/SOURCEDATA/sub-${group}${subjID}/ses-first/anat/*.nii.gz ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/
+#pydeface  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_T1.nii.gz
+#pydeface  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_T2.nii.gz
+rm  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_T1.nii.gz
+rm  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_T2.nii.gz
+mv  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_run-01_T1_defaced.nii.gz ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/sub-${group}${subjID}_ses-first_run-01_T1w.nii.gz
+mv  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_run-01_T2_defaced.nii.gz ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/sub-${group}${subjID}_ses-first_run-01_T2w.nii.gz
 
-  #new directory with final preprocesssed bold files
-    #cp ${home}/REWOD/DERIVATIVES/PREPROC/sub-${subjID}/ses-second/func/task-${taskID}.ica/filtered_func_data_clean_unwarped_Coreg.nii.gz ${home}/REWOD/DERIVATIVES/PREPROC/sub-${subjID}/ses-second/func/sub-${subjID}_task-${taskID}_run-01_UnsmoothedBold.nii.gz
-  #cd ${home}/REWOD/DERIVATIVES/ANALYSIS/MVPA/hedonic/MVPA-04/sub-${subjID}/mvpa/
-  #fslstats svm_smell_nosmell_smoothed.nii -m -M
-  #echo 'done'
+
+
+
+
     #done
- done
+ #done
--- BIDS/run_calc.sh
+++ BIDS/run_calc.sh
@@ -2,13 +2,26 @@
 home=$(eval echo ~$user)
 
 
-#Script=${home}/REWOD/CODE/BIDS/mriQC.sh
-Script=${home}/REWOD/CODE/BIDS/copy2.sh
 
-# Loop over subjects
- for subjectID in 01 #02 03 04 05 06 07 09 10 11 12 13 14 15 16 17 18 20 21 22 23 24 25 26 #
+#group='control1'
+group='obese2'
+
+#Script=${home}/REWOD/CODE/BIDS/mriQC.sh
+Script=${home}/OBIWAN/CODE/BIDS/copy2.sh
+
+#subjID=$1
+#group=$2
+
+# Loop over control1: 00	01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25	26	27	28	29	30	31	32	33
+#obese2: 00	01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25	26	27	28	29	30	31	32	33	34	35	36	37	38	39	40	41	42	44	45	46	47	48	49	50	51	52	53	54	56	58	59	61	62	63	64	65	66	67	68	69	70
+ #missing 43 55 57 60
+ for subjectID in 00 01 02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25	26	27	28	29	30	31	32	33	34	35	36	37	38	39	40	41	42	44	45	46	47	48	49	50	51	52	53	54	56	58	59	61	62	63	64	65	66	67	68	69	70
+ #for subjectID in 00 01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25	26	27	28	29	30	31	32	33
     do
     #qsub -o ${home}/REWOD/ClusterOutput -j oe -l walltime=44:00:00,pmem=4GB -M david.munoz@etu.unige.ch -m n -l nodes=1 -q queue1 -N CP_sub-${subjectID} -F "${subjectID}"  ${Script}
-    qsub -o ${home}/REWOD/ClusterOutput -j oe -l walltime=84:00:00,pmem=16GB -M david.munoz@etu.unige.ch -m n -l nodes=1 -q queue1 -N MRIQC ${Script}
+    #pydeface  ${home}/OBIWAN/sub-${group}${subjectID}/ses-first/anat/*_T1.nii.gz
+    #pydeface  ${home}/OBIWAN/sub-${group}${subjectID}/ses-first/anat/*_T2.nii.gz
+    #echo 'done'
+    qsub -o ${home}/REWOD/ClusterOutput -j oe -l walltime=01:00:00,pmem=4GB -M david.munoz@etu.unige.ch -m n -l nodes=1 -q queue1 -N copy_${group}${subjectID} -F "${subjectID} ${group}" ${Script}
 
 done
