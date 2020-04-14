a21f7da75e999d5015ecc1bbd5d632ed82fface5
munoztd0 <david.munoz@etu.unige.ch>
2020-04-14 18:22:16 +0200

    clean

--- BIDS/copy2.sh
+++ BIDS/copy2.sh
@@ -2,11 +2,13 @@
 home=$(eval echo ~$user)
 
 #small function to move and copy files
-subjID=$1
-group=$2
+#subjID=$1
+#group=$2
+#group='obese2'
+group='control1'
 
-source ~/anaconda3/etc/profile.d/conda.sh 
-conda activate NEW
+#source ~/anaconda3/etc/profile.d/conda.sh 
+#conda activate NEW
 
 
 #for subjID in 01 02 03 04 05 06 07 09 10 11 12 13 14 15 16 17 18 20 21 22 23 24 25 26
@@ -23,11 +25,16 @@ conda activate NEW
 #cp ${home}/OBIWAN/DATA/STUDY/SOURCEDATA/sub-${group}${subjID}/ses-first/anat/*.nii.gz ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/
 #pydeface  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_T1.nii.gz
 #pydeface  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_T2.nii.gz
-rm  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_T1.nii.gz
-rm  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_T2.nii.gz
-mv  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_run-01_T1_defaced.nii.gz ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/sub-${group}${subjID}_ses-first_run-01_T1w.nii.gz
-mv  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_run-01_T2_defaced.nii.gz ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/sub-${group}${subjID}_ses-first_run-01_T2w.nii.gz
-
+#rm  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_T1.nii.gz
+#rm  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_T2.nii.gz
+#mv  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_run-01_T1_defaced.nii.gz ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/sub-${group}${subjID}_ses-first_run-01_T1w.nii.gz
+#mv  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_run-01_T2_defaced.nii.gz ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/sub-${group}${subjID}_ses-first_run-01_T2w.nii.gz
+#OB for subjID in 01 02	03	04	05	06	07	08	09	10	11	12	13	14	15 16	17	18	19	20	21	22	23	24	25	26	27	28	29	30	31	32	33	34	35	36	37	38	39	40	41	42	44	45	46	47	48	49	50	51	52	53	54	56	58	59	61	62	63	64	65	66	67	68	69	70
+for subjID in 00 01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20	21	22	23	24	25	26	27	28	29	30	31	32	33
+  do
+  mv  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_run-01_T1.json ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/sub-${group}${subjID}_ses-first_run-01_T1w.json
+  mv  ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/*_run-01_T2.json ${home}/OBIWAN/sub-${group}${subjID}/ses-first/anat/sub-${group}${subjID}_ses-first_run-01_T2w.json
+done
 
 
 
--- BIDS/mriQC.sh
+++ BIDS/mriQC.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-mriqc ~/REWOD/ ~/REWOD/QualityCheck participant #--participant-label 01
-#mriqc ~/OBIWAN/DATA/STUDY/SOURCEDATA/ ~/OBIWAN/DATA/STUDY/SOURCEDATA/QualityCheck participant #--participant-label control100
+home=$(eval echo ~$user)
+
+
+subjID=$1
+group=$2
+
+
+cd ${home}/OBIWAN/DERIVATIVES/MRIQC/
+
+
+#mriqc ${home}/REWOD/ ${home}/REWOD/QualityCheck participant #--participant-label 01
+
+for subjectID in 09	10	11	12	13	14	15 16
+    do
+    mriqc ${home}/OBIWAN/DATA/STUDY/SOURCEDATA/ ${home}/OBIWAN/DERIVATIVES/MRIQC participant --participant-label ${group}${subjectID}
+done
